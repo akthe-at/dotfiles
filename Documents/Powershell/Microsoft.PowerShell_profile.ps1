75ecb13969e3f73f2a847a9acba182e00ba2dd60
Adam K. <arkelly2@medicine.wisc.edu>
2024-04-12 19:50:15 -0500

    powershell profiles/scripts

---
+++ Documents/Powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,122 @@
+function Set-StyleScriptAlias
+{
+  [alias("StyleScript")]
+  param(
+    [string[]]$file
+  )
+  # Call R to style the file
+  Rterm.exe --quiet -e "styler::style_file('$file')" 2>&1
+
+}
+
+$global:profile_initialized = $false
+
+# region Copied from Jaykul
+trap
+{ Write-Warning ($_.ScriptStackTrace | Out-String) 
+}
+# $InformationPreference = "Continue"
+# I wish $Profile was in $Home, but since it's not:
+$ProfileDir = $PSScriptRoot
+
+$DataHome = if ($ENV:XDG_CONFIG_HOME)
+{
+  $ENV:XDG_DATA_HOME
+} else
+{
+  [IO.Path]::Combine($HOME, "Documents")
+}
+
+
+if ($Host.UI.RawUI.KeyAvailable)
+{
+  $Controlled = $false
+  while ($Host.UI.RawUI.KeyAvailable -and ($key = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown,IncludeKeyUp")))
+  {
+    if (!$Controlled -and $key.ControlKeyState -match "LeftCtrlPressed")
+    {
+      $Controlled = $true
+    }
+  }
+  if ($Controlled)
+  {
+    Write-Host "Skipping Interactive Config. To complete, run:`n. `"$Interactive`"" -ForegroundColor Yellow
+    if ($PSVersionTable.PSVersion.Major -gt 5)
+    {
+      function prompt
+      { "`e[36m$($MyInvocation.HistoryId)`e[37m $pwd`e[0m`n❯" 
+      }
+    } else
+    {
+      function prompt
+      { "$([char]27)[36m$($MyInvocation.HistoryId)$([char]27)[37m $pwd$([char]27)[0m`n$([char]0x276f)" 
+      }
+    }
+    return
+  }
+}
+# regionend
+
+
+
+# Load extra functions
+@(
+  "Functions.ps1",
+  "ShellIntegration.ps1",
+  "Initialize-Profile.ps1"
+) | ForEach-Object {
+  $path = "$DataHome/Powershell/Scripts/$_"
+  if (Test-Path $path -ErrorAction SilentlyContinue)
+  {
+    . (Convert-Path $path)
+  }
+}
+
+
+
+# Starship overwrite the prompt. Do this so its available on first open.
+# There is a cost for this but it should be minimal.
+if (Get-Command 'starship' -ErrorAction SilentlyContinue)
+{
+  Invoke-Expression (&starship init powershell)
+  function Invoke-Starship-PreCommand
+  {
+    if ($global:profile_initialized -ne $true)
+    {
+      $global:profile_initialized = $true
+      Initialize-Profile
+    }
+  }
+
+
+  # Update previous prompt with simpler prompt
+  function Invoke-Starship-TransientFunction
+  {
+    &starship prompt --profile short
+  }
+  Enable-TransientPrompt
+} else
+{
+  Write-Host "Preparing interactive session for first use..." -ForegroundColor Cyan
+  function prompt
+  {
+    . (Convert-Path "$DataHome/powershell/Scripts/OldPrompt")
+    if ($global:profile_initialized -ne $true)
+    {
+      $global:profile_initialized = $true
+      Initialize-Profile
+    }
+  }
+}
+
+Invoke-Expression (&starship init powershell)
+
+
+# oh-my-posh.exe init pwsh --config "$env:POSH_THEMES_PATH\catppuccin.omp.json" | Invoke-Expression
+Set-PSReadLineOption -PredictionViewStyle InlineView
+# set wezterm as the TERM env
+# $Env:TERM = "wezterm"
+$Env:KOMOREBI_CONFIG_HOME = 'C:\Users\ARK010\.config\komorebi'
+$Env:WHKD_CONFIG_HOME = 'C:\Users\ARK010\.config\komorebi'
+
+
