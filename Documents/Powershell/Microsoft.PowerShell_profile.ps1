6945d2a01b09ac7586dcedbf1483bb070f950d16
Adam K. <arkelly2@medicine.wisc.edu>
2024-08-02 22:25:53 -0500

    update PS profile

--- Documents/Powershell/Microsoft.PowerShell_profile.ps1
+++ Documents/Powershell/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,47 @@
-function cd... { Set-Location ..\.. }
-function cd.... { Set-Location ..\..\.. }
+#Set-PSDebug -Trace 1
 
-function touch($file) {
-    "" | Out-File $file -Encoding ASCII
+#function prdb
+#{
+#  param (
+#    [Parameter(ValueFromRemainingArguments=$true)]
+#    $args
+#  )
+#  # Store the current location
+#  $initialPath = Get-Location
+#  Set-Location -Path "~/Documents/DEM/PeerReview/sqlite_loader/Python"
+#  ape
+#  set-Location $initialPath
+#  python "~/Documents/DEM/PeerReview/sqlite_loader/Python/src/prdb/"
+#  python load_sent_cases.py $args
+#  deactivate
+#}
+
+function gwt
+{
+  & '~/.scripts/create-wt.ps1' @args
+}
+
+function cd($path)
+{
+  z $path
+}
+
+function yy
+{
+  $tmp = [System.IO.Path]::GetTempFileName()
+  yazi $args --cwd-file="$tmp"
+  $cwd = Get-Content -Path $tmp
+  if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path)
+  {
+    Set-Location -LiteralPath $cwd
+  }
+  Remove-Item -Path $tmp
+}
+
+
+function touch($file)
+{
+  "" | Out-File $file -Encoding ASCII
 }
 
 function Set-StyleScriptAlias
@@ -26,8 +65,21 @@ trap
 # I wish $Profile was in $Home, but since it's not:
 $ProfileDir = $PSScriptRoot
 
-$DataHome = [IO.Path]::Combine($HOME, "Documents")
-
+# The XDG standard says use the variable and tells us how to calculate a fallback
+$DataHome = if ($ENV:XDG_CONFIG_HOME)
+{
+  $ENV:XDG_DATA_HOME
+} else
+{
+  [IO.Path]::Combine($HOME, "Documents")
+}
+$ConfigHome = if ($ENV:XDG_CONFIG_HOME)
+{
+  $ENV:XDG_CONFIG_HOME
+} else
+{
+  [IO.Path]::Combine($HOME, ".config")
+}
 
 if ($Host.UI.RawUI.KeyAvailable)
 {
@@ -56,67 +108,159 @@ if ($Host.UI.RawUI.KeyAvailable)
     return
   }
 }
-# regionend
-
-
 
 # Load extra functions
 @(
   "Functions.ps1",
   "ShellIntegration.ps1",
-  "Initialize-Profile.ps1"
+  "Initialize-Profile.ps1",
+  "GitTools.ps1"
 ) | ForEach-Object {
-  $path = "$DataHome/Powershell/Scripts/$_"
-  if (Test-Path $path -ErrorAction SilentlyContinue)
-  {
-    . (Convert-Path $path)
-  }
+  $path = "~/Documents/Powershell/Scripts/$_"
+  . (Convert-Path $path)
 }
 
 
 
-# Starship overwrite the prompt. Do this so its available on first open.
-# There is a cost for this but it should be minimal.
-if (Get-Command 'starship' -ErrorAction SilentlyContinue)
+### Starship overwrite the prompt. Do this so its available on first open.
+### There is a cost for this but it should be minimal.
+#if (Get-Command 'starship' -ErrorAction SilentlyContinue)
+#{
+#  Invoke-Expression (&starship init powershell)
+#  function Invoke-Starship-PreCommand
+#  {
+#    if ($global:profile_initialized -ne $true)
+#    {
+#      $global:profile_initialized = $true
+#      Initialize-Profile
+#    }
+#  }
+#  # Update previous prompt with simpler prompt 
+#  function Invoke-Starship-TransientFunction
+#  {
+#    &starship prompt --profile short
+#  }
+#  Enable-TransientPrompt
+#} else
+#{
+#  Write-Host "Preparing interactive session for first use..." -ForegroundColor Cyan
+#  function prompt
+#  {
+#    . (Convert-Path "$DataHome/Powershell/Scripts/OldPrompt")
+#    if ($global:profile_initialized -ne $true)
+#    {
+#      $global:profile_initialized = $true
+#      Initialize-Profile
+#    }
+#  }
+#}
+########
+# OH MY POSH SECTION
+########
+
+Invoke-Expression (oh-my-posh init pwsh --config "C:\Users\ARK010\.config\ohmyposh\rosepine.toml")
+#if (Get-Command 'oh-my-posh' -ErrorAction SilentlyContinue)
+#{
+#  Invoke-Expression (oh-my-posh init pwsh --config "C:\Users\ARK010\.config\ohmyposh\rosepine.toml")
+#  if ($global:profile_initialized -ne $true)
+#  {
+#    $global:profile_initialized = $true
+#    Initialize-Profile
+#  }
+#} else
+#{
+#  Write-Host "'oh-my-posh' command not found. The default prompt will be used." -ForegroundColor Red
+#}
+
+# Utility Functions
+function Test-CommandExists
 {
-  Invoke-Expression (&starship init powershell)
-  function Invoke-Starship-PreCommand
-  {
-    if ($global:profile_initialized -ne $true)
-    {
-      $global:profile_initialized = $true
-      Initialize-Profile
-    }
-  }
+  param($command)
+  $exists = $null -ne (Get-Command $command -ErrorAction SilentlyContinue)
+  return $exists
+}
 
-
-  # Update previous prompt with simpler prompt
-  function Invoke-Starship-TransientFunction
-  {
-    &starship prompt --profile short
-  }
-  Enable-TransientPrompt
+$EDITOR = if (Test-CommandExists nvim)
+{ 'nvim' 
+} elseif (Test-CommandExists vim)
+{ 'vim' 
+} elseif (Test-CommandExists vi)
+{ 'vi' 
+} elseif (Test-CommandExists code)
+{ 'code' 
+} elseif (Test-CommandExists notepad++)
+{ 'notepad++' 
 } else
+{ 'notepad' 
+}
+Set-Alias -Name vim -Value $EDITOR
+
+# Network Utilities
+function Get-PubIP
+{ (Invoke-WebRequest http://ifconfig.me/ip).Content 
+}
+
+function uptime
 {
-  Write-Host "Preparing interactive session for first use..." -ForegroundColor Cyan
-  function prompt
+  if ($PSVersionTable.PSVersion.Major -eq 5)
   {
-    . (Convert-Path "$DataHome/powershell/Scripts/OldPrompt")
-    if ($global:profile_initialized -ne $true)
-    {
-      $global:profile_initialized = $true
-      Initialize-Profile
+    Get-WmiObject win32_operatingsystem | Select-Object @{Name='LastBootUpTime'; Expression={$_.ConverttoDateTime($_.lastbootuptime)}} | Format-Table -HideTableHeaders
+  } else
+  {
+    net statistics workstation | Select-String "since" | ForEach-Object { $_.ToString().Replace('Statistics since ', '') }
+  }
+}
+
+
+function docs
+{ Set-Location -Path $HOME\Documents 
+}
+
+function dtop
+{ Set-Location -Path $HOME\Desktop 
+}
+
+# Quick Access to Editing the Profile
+function ep
+{ nvim $PROFILE 
+}
+
+#zebar
+function zb([string]$arg)
+{
+  switch ($arg)
+  {
+    "stop"
+    { Stop-Process -Name zebar -ErrorAction SilentlyContinue 
+    }
+    "start"
+    { 
+      & "$env:ZEBAR_CONFIG_HOME\start.ps1"
+    }
+    "restart"
+    { zb "stop"; zb "start"
     }
   }
 }
 
-Invoke-Expression (&starship init powershell)
+function zi
+{
+  param($param)
+  $_zoxide_result = $(zoxide query -i -- $param)
+  if ($_zoxide_result)
+  {
+    Set-Location $_zoxide_result
+  }
+}
 
-# oh-my-posh.exe init pwsh --config "$env:POSH_THEMES_PATH\catppuccin.omp.json" | Invoke-Expression
-Set-PSReadLineOption -PredictionViewStyle InlineView
+function yasb 
+{
+  C:\Users\ARK010\.scripts\start-yasb.ps1
+}
+# oh-my-posh.exe init pwsh --config "$env:POSH_THEMES_PATH\gruvbox.omp.json" | Invoke-Expression
 # set wezterm as the TERM env
 # $Env:TERM = "wezterm"
-# $Env:KOMOREBI_CONFIG_HOME = 'C:\Users\ARK010\.config\komorebi'
-# $Env:WHKD_CONFIG_HOME = 'C:\Users\ARK010\.config\komorebi'
+$Env:KOMOREBI_CONFIG_HOME = 'C:\Users\ARK010\.config\komorebi'
+$Env:WHKD_CONFIG_HOME = 'C:\Users\ARK010\.config\komorebi'
 
-
+#Set-PSDebug -Trace 0
