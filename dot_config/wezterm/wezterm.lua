a6e57797bebe25be901fbb9f0e1c9231f2ac3ab2
Adam K. <arkelly2@medicine.wisc.edu>
2025-01-21 11:09:13 -0600

    remove

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,8 +1,8 @@
 local Config = require("utils.class.config"):new()
 
-require "events.update-status"
-require "events.format-tab-title"
+require("events.update-status")
+require("events.format-tab-title")
 require("utils.backdrops"):set_files():random()
-require "events.augment-command-palette"
+require("events.augment-command-palette")
 
-return Config:add("config"):add "mappings"
+return Config:add("config"):add("mappings")
