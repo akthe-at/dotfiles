3c9d70d672bc1965697b1206ebd3be6f93bc0386
Adam K. <arkelly2@medicine.wisc.edu>
2025-01-21 09:39:01 -0600

    update sessionizer script

--- dot_config/wezterm/sessionizer.lua
+++ dot_config/wezterm/sessionizer.lua
@@ -1,6 +1,6 @@
-local wt = require "wezterm"
+local wt = require("wezterm")
 local act = wt.action
-local fun = require "utils.fn"
+local fun = require("utils.fn")
 local M = {}
 
 local home = "//wsl.localhost/Ubuntu-24.04/home/aktheat/"
@@ -9,44 +9,42 @@ local home = "//wsl.localhost/Ubuntu-24.04/home/aktheat/"
 ---@param name string|boolean|nil
 ---@param err_message string
 local function err_if_not(name, err_message)
-  if not name then
-    wt.log_error(err_message)
-  end
+	if not name then
+		wt.log_error(err_message)
+	end
 end
 --
 --- path if file or directory exists nil otherwise
 ---@param path string
 local function file_exists(path)
-  if path == nil then
-    return nil
-  end
-  local f = io.open(path, "r")
-  -- io.open won't work to check if directories exist,
-  -- but works for symlinks and regular files
-  if f ~= nil then
-    wt.log_info(path .. " file or symlink found")
-    io.close(f)
-    return path
-  end
-  return nil
+	if path == nil then
+		return nil
+	end
+	local f = io.open(path, "r")
+	-- io.open won't work to check if directories exist,
+	-- but works for symlinks and regular files
+	if f ~= nil then
+		wt.log_info(path .. " file or symlink found")
+		io.close(f)
+		return path
+	end
+	return nil
 end
 
 -------------------------------------------------------
 -- PATHS
 --
 local fd = (
-  file_exists "C:\\Users\\aktheat\\scoop\\shims\\fd.exe"
-  or file_exists(home .. "/usr/bin/fd")
-  or file_exists "usr/bin/fdfind"
-  or file_exists "usr/bin/fdfind.exe"
-  or file_exists(home .. "/bin/fd.exe")
+	file_exists("C:\\Users\\aktheat\\scoop\\shims\\fd.exe")
+	or file_exists(home .. "/usr/bin/fd")
+	or file_exists("usr/bin/fdfind")
+	or file_exists("usr/bin/fdfind.exe")
+	or file_exists(home .. "/bin/fd.exe")
 )
 
 err_if_not(fd, "fd not found")
 
-local git = (
-  file_exists "C:\\Program Files\\Git\\cmd\\git.exe" or file_exists "/usr/bin/git"
-)
+local git = (file_exists("C:\\Program Files\\Git\\cmd\\git.exe") or file_exists("/usr/bin/git"))
 err_if_not(git, "git not found")
 
 -- local mydocs = home .. "\\Documents"
@@ -57,60 +55,57 @@ local config = "C:/Users/aktheat/.config"
 -------------------------------------------------------
 
 M.start = function(window, pane)
-  local projects = {}
-  local success, stdout, stderr = wt.run_child_process {
-    fd,
-    "-HI",
-    ".git$",
-    "--max-depth=4",
-    "--prune",
-    -- mydocs,
-    dem,
-    plugins,
-    config,
-  }
+	local projects = {}
+	local success, stdout, stderr = wt.run_child_process({
+		fd,
+		"-HI",
+		".git$",
+		"--max-depth=4",
+		"--prune",
+		-- mydocs,
+		dem,
+		plugins,
+		config,
+	})
 
-  if not success then
-    wt.log_error("Failed to run fd: " .. stderr)
-    return
-  end
+	if not success then
+		wt.log_error("Failed to run fd: " .. stderr)
+		return
+	end
 
-  for line in stdout:gmatch "([^\n]*)\n?" do
-    -- create label from file path
-    local project = line:gsub("/.git.*", ""):gsub("\\.git.*", "")
-    project = project:gsub("/$", ""):gsub("\\$", ""):gsub("\\", "/")
-    local label = project:gsub("//wsl%.localhost/Ubuntu%-24%.04/home/aktheat/", "~")
-    wt.log_info("Found project: " .. label)
+	for line in stdout:gmatch("([^\n]*)\n?") do
+		-- create label from file path
+		local project = line:gsub("/.git.*", ""):gsub("\\.git.*", "")
+		project = project:gsub("/$", ""):gsub("\\$", ""):gsub("\\", "/")
+		local label = project:gsub("//wsl%.localhost/Ubuntu%-24%.04/home/aktheat/", "~")
+		wt.log_info("Found project: " .. label)
 
-    -- extract id. Used for workspace name
-    local _, _, id = string.find(project, ".*/(.+)")
-    id = id:gsub(".git", "") -- bare repo dirs typically end in .git, remove if so.
+		-- extract id. Used for workspace name
+		local _, _, id = string.find(project, ".*/(.+)")
+		id = id:gsub(".git", "") -- bare repo dirs typically end in .git, remove if so.
 
-    wt.log_info("FOUND ID PROJECT: " .. id)
-    table.insert(projects, { label = tostring(label), id = tostring(id) })
-  end
+		wt.log_info("FOUND ID PROJECT: " .. id)
+		table.insert(projects, { label = tostring(label), id = tostring(id) })
+	end
 
-  window:perform_action(
-    act.InputSelector {
-      action = wt.action_callback(function(window, pane, id, label)
-        if not id and not label then
-          wt.log_info "Cancelled"
-        else
-          wt.log_info("Selected " .. label)
-          local pretty_id = id:match ".*\\(.*)" -- This line is added
+	window:perform_action(
+		act.InputSelector({
+			action = wt.action_callback(function(window, pane, id, label)
+				if not id and not label then
+					wt.log_info("Cancelled")
+				else
+					wt.log_info("Selected " .. label)
+					local pretty_id = id:match(".*\\(.*)") -- This line is added
 
-          window:perform_action(
-            act.SwitchToWorkspace { name = id, spawn = { cwd = label } },
-            pane
-          )
-        end
-      end),
-      fuzzy = true,
-      title = "Select project",
-      choices = projects,
-    },
-    pane
-  )
+					window:perform_action(act.SwitchToWorkspace({ name = id, spawn = { cwd = label } }), pane)
+				end
+			end),
+			fuzzy = true,
+			title = "Select project",
+			choices = projects,
+		}),
+		pane
+	)
 end
 
 return M
